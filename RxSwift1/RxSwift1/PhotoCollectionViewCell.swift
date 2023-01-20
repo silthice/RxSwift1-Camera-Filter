220076d59ae7b8367a12f458a524c94080daec0d
silthice <ryonlim@hotmail.com>
2023-01-20 18:28:56 +0800

    -Debug collectionView crash

---
+++ RxSwift1/RxSwift1/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  RxSwift1
+//
+//  Created by Giap on 20/01/2023.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+}
--- RxSwift1/RxSwift1/PhotoCollectionViewController.swift
+++ RxSwift1/RxSwift1/PhotoCollectionViewController.swift
@@ -7,7 +7,58 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotoCollectionViewController: UICollectionViewController {
     
+    private var images = [PHAsset]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else { fatalError("PhotoCollectionViewCell is not found") }
+            
+        
+        let asset = self.images[indexPath.row]
+        //Using PHImageManager singleton to assign asset images to image view
+        let manager = PHImageManager.default()
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+        }
+        
+        return cell
+
+    }
+    
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+            if status == .authorized {
+                // access photo from photo library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                assets.enumerateObjects { (object, count, stop) in
+                    self?.images.append(object)
+                }
+                //show recent image at top
+                self?.images.reverse()
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+                
+            }
+        }
+    }
 }
